93541a8981dcc98911a6a174d77548312a3e6ec1
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2020-08-16 22:33:26 +0900

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Main {
+
+  public static void main(String[] args) {
+    List<Person> people = getPeople();
+
+    // Imperative approach ❌
+    /*
+    List<Person> females = new ArrayList<>();
+
+    for (Person person : people) {
+      if (person.getGender().equals(Gender.FEMALE)) {
+        females.add(person);
+      }
+    }
+    females.forEach(System.out::println);
+    */
+
+    // Declarative approach ✅
+  }
+  private static List<Person> getPeople() {
+    return List.of(
+        new Person("Antonio", 20, Gender.MALE),
+        new Person("Alina Smith", 33, Gender.FEMALE),
+        new Person("Helen White", 57, Gender.FEMALE),
+        new Person("Alex Boz", 14, Gender.MALE),
+        new Person("Jamie Goa", 99, Gender.MALE),
+        new Person("Anna Cook", 7, Gender.FEMALE),
+        new Person("Zelda Brown", 120, Gender.FEMALE)
+    );
+  }
+
+}
